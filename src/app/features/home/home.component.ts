2dc01256c0832b07fc153e8a5317d76c9a290547
Shuang Wu <seanwu1105@gmail.com>
2022-06-12 17:31:52 -0500

    Populate data from csv & preview with embedded PDF

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -5,6 +5,18 @@ import { MatDividerModule } from '@angular/material/divider'
 import { MatIconModule } from '@angular/material/icon'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { DomSanitizer } from '@angular/platform-browser'
+import { parse } from 'csv/browser/esm/sync'
+import { jsPDF } from 'jspdf'
+import {
+  BehaviorSubject,
+  combineLatest,
+  distinctUntilChanged,
+  filter,
+  map,
+  Observable,
+  switchMap,
+} from 'rxjs'
 
 @Component({
   selector: 'app-home',
@@ -21,18 +33,61 @@ import { MatToolbarModule } from '@angular/material/toolbar'
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  data = ''
-  template = ''
+  private readonly data$ = new BehaviorSubject('')
+
+  private readonly csv$: Observable<Record<string, string>[]> = this.data$.pipe(
+    map(data => parse(data, { columns: true }))
+  )
+
+  private readonly template$ = new BehaviorSubject('')
+
+  private readonly pageNumber$ = new BehaviorSubject(0)
+
+  private readonly populated$ = combineLatest([this.csv$, this.template$]).pipe(
+    distinctUntilChanged(),
+    filter(([csv, template]) => csv.length > 0 && template.length > 0),
+    map(([csv, template]) =>
+      csv.map(record => {
+        let result = template
+        Object.keys(record).forEach(key => {
+          result = result.replace(`[[${key}]]`, record[key])
+        })
+        return result
+      })
+    )
+  )
+
+  readonly pdf$ = combineLatest([this.populated$, this.pageNumber$]).pipe(
+    distinctUntilChanged(),
+    filter(
+      ([populated, pageNumber]) =>
+        populated.length > 0 && pageNumber < populated.length
+    ),
+    switchMap(([populated, pageNumber]) => generatePdf(populated[pageNumber])),
+    map(url => this.sanitizer.bypassSecurityTrustResourceUrl(url.toString()))
+  )
+
+  constructor(private readonly sanitizer: DomSanitizer) {}
 
   async onDataChange(e: Event) {
-    this.data = await readFileInputEvent(e)
-    console.log(this.data)
+    this.data$.next(await readFileInputEvent(e))
   }
 
   async onTemplateChange(e: Event) {
-    this.template = await readFileInputEvent(e)
-    console.log(this.template)
+    this.template$.next(await readFileInputEvent(e))
   }
+}
+
+async function generatePdf(html: string) {
+  const A4_WIDTH_PX = 800
+  return new Promise<URL>((resolve, _) => {
+    const doc = new jsPDF({ unit: 'px', hotfixes: ['px_scaling'] })
+    doc.html(html, {
+      callback: doc => resolve(doc.output('bloburl')),
+      windowWidth: A4_WIDTH_PX,
+      width: A4_WIDTH_PX,
+    })
+  })
 }
 
 async function readFileInputEvent(e: Event) {
