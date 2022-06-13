7da087ef2cfd409fc976642fc30ae1700c1b4797
Shuang Wu <seanwu1105@gmail.com>
2022-06-12 20:15:54 -0500

    Fix default tests

--- src/app/features/home/home.component.spec.ts
+++ src/app/features/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { HomeComponent } from './home.component'
 
@@ -8,7 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent],
+      imports: [HomeComponent, BrowserAnimationsModule],
     }).compileComponents()
 
     fixture = TestBed.createComponent(HomeComponent)
